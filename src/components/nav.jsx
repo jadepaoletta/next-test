54a153277325b588a333fd4419408eb931dfbd2e
jadepaoletta <31874306+jadepaoletta@users.noreply.github.com>
2023-11-01 14:55:54 -0700

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 const Nav = ()=>{
     return(
         <div className="grid grid-cols-4 px-20 py-16 text-center">
-            <h1 className="hover:text-slate-500"><Link href="/">Home</Link></h1>
-            <h1 className="hover:text-slate-500"><Link href="/about">About</Link></h1>
-            <h1 className="hover:text-slate-500"><Link href="/test-page">Test Page</Link></h1>
+            <h1 className="hover:text-slate-500"><Link href="/"><a>Home</a></Link></h1>
+            <h1 className="hover:text-slate-500"><Link href="/about"><a>About</a></Link></h1>
+            <h1 className="hover:text-slate-500"><Link href="/test-page"><a>test</a></Link></h1>
         </div>
     )
 }
