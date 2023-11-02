ff3bff1921ccd5f78821e39f2b43c8078400bd61
jadepaoletta <jadepaoletta27@gmail.com>
2023-11-01 17:34:42 -0700

    update component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/header'
-import Nav from '@/components/nav'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         < Header />
-        < Nav />
+        <h1 className="hover:text-slate-500"><Link href="/">Home</Link></h1>
+        <h1 className="hover:text-slate-500"><Link href="/about">About</Link></h1>
+        <h1 className="hover:text-slate-500"><Link href="/test-page">Test Page</Link></h1>
         {children}
       </body>
     </html>
