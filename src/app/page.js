f43c48a8f56eed950e9607b694e15e11933c4bea
jadepaoletta <jadepaoletta27@gmail.com>
2023-11-16 15:48:47 -0800

    redis client

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,19 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { createClient } from "redis";
+
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    family: 6,
+  },
+})
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .connect();
+
+  
+await client.set('key', 'value');
+const value = await client.get('key');
+await client.disconnect();
 
 export default function Home() {
   return (
@@ -6,5 +21,5 @@ export default function Home() {
       Home page
       <Link href="/about">About</Link>
     </div>
-  )
+  );
 }
